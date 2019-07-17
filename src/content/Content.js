4b6907a0b5ea4c429856803d4efe02a75392d280
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-17 14:48:43 -0700

    finish styling the rest of the layout

--- src/content/Content.js
+++ src/content/Content.js
@@ -5,8 +5,10 @@ import Media from './media/Media';
 function Content({ title, text, type, url }) {
   return (
     <section>
+      <div className="content">
         <Preamble text={text}/>
         <Media title={title} type={type} url={url}/>
+      </div>
     </section>
   );
 }
