78a24991ed87cbec8908291b5c441daf116e1260
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-17 13:46:29 -0700

    add cache for api requests

--- src/App.js
+++ src/App.js
@@ -8,17 +8,24 @@ import DatePicker from './date-picker/DatePicker';
 
 function App() {
   const apiKey = '251h2uaJvdQrHzVG2pxwAUU2SXUoX1OIPxqI7F58';
+  const [cache, setCache] = useState({});
   const [date, setDate] = useState(new Date().toLocaleDateString('en-CA'));
   const [photo, setPhoto] = useState({});
 
   useEffect(() => {
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
-      .then(res => {
-        setPhoto(res.data);
-        console.log(res);
-      });
-  }, [date]);
+
+    if (cache[date]) {
+      setPhoto(cache[date]);
+    } else {
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
+        .then(res => {
+          setPhoto(res.data);
+          setCache(cache => ({...cache, [date]: res.data}));
+          console.log(res);
+        });
+    }
+  }, [cache, date]);
 
   return (
     <div className="App">
